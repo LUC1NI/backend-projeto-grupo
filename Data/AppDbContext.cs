9f575c989bda8d61aed7842aebb5810377c8feaa
Leonardo Reis <leonardoseletiva0106@gmail.com>
2025-05-01 13:31:50 -0300

    Melhorando o AppDbContext

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,6 +1,13 @@
 using Microsoft.EntityFrameworkCore;
-using BackendProjetoGrupo;
+using BackendProjeto.Models;
 
-namespace BackendProjetoGrupo.Data
+namespace BackendProjeto.Data
 {
-    
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) {}
+
+        public DbSet<Filme> Filmes { get; set; }
+        public DbSet<Genero> Generos { get; set; }
+    }
+}
